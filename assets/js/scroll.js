551fbb281994a16db9adafa46853d347d40adc87
Junhyung Cho <choms46@me.com>
2018-05-31 01:38:20 +0900

    Add Scrollsnap with JS

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,21 +1,6 @@
-
-$(function() {
-$.scrollify({
-    section : ".pages",
-    sectionName : "section-name",
-    interstitialSection : "",
-    easing: "easeOutExpo",
-    scrollSpeed: 800,
-    offset : 0,
-    scrollbars: true,
-    standardScrollElements: "",
-    setHeights: true,
-    overflowScroll: false,
-    updateHash: true,
-    touchScroll:true,
-    before:function() {},
-    after:function() {},
-    afterResize:function() {},
-    afterRender:function() {}
-  });
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        navigation: true,
+        navigationPosition: 'left',
+    });
 });
