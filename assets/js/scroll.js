c2e1f4741fd4e06bcd0cd88fabba482ba72c5b71
Junhyung Cho <choms46@me.com>
2018-05-31 17:58:47 +0900

    폰트 추가 및 5번 슬라이드 이미지 변경

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,16 @@
 var posterTitle = [
     
-    'title1, name1, 2018',
-    'title2, name2, 2018',
-    'title3, name3, 2018',
-    'title4, name4, 2018',
-    'title5, name5, 2018'
+    'Poster series for The Wanderlust by Swiss designer Felix Pfäffli, 2014',
+    'Poster for Two&2t by Brenttton on Apr 9, 2017',
+    'Posted by GRAPHIC DESIGN & SILKSCREEN',
+    'Posted by Maan Ali - Haikuglyphics on Apr 14, 2017',
+    "Posted by GRAPHIC DESIGNER grafikr's inspiration"
     
 ];
 
 var fontColor = ['black', 'black', 'white', 'black', 'white'];
-var fontFamily = ['Montserrat','Monospace','Serief','Montserrat','Monospace'];
+var fontFamily = ['Nanum Gothic','Roboto','Arial Black','Avenir Next Condensed','PT Sans Caption'];
+var fontWeight = ['Extra-Bold','','Regular','',''];
 
 $(document).ready(function() {
     $('#fullpage').fullpage({
@@ -26,13 +27,16 @@ $(document).ready(function() {
             $('#fontname').remove();
             
 //          append font name            
-            $('#appendFontname').append('<div id="fontname" class="col align-text-bottom" style="">'+fontFamily[index-1]+'</div>');
+            $('#appendFontname').append('<div id="fontname" class="col align-text-bottom" style="">'+fontFamily[index-1] + ' ' + fontWeight[index-1] +'</div>');
             
 //          change font family
             $('#fontname').css('font-family', fontFamily[index-1]);
             
 //          change font color
             $('#removeText').css('color', fontColor[index-1]);
+            
+//          change font weight
+            $('#fontname').css('font-weight', fontWeight[index-1]);
             
         }
     });
